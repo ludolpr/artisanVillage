6b471071064403893445a5f221befdd48a03a1e7
ludolpr <ludolpr@gmail.com>
2024-11-06 14:13:38 +0100

    env correctly masked

--- src/services/baseUrl/index.jsx
+++ src/services/baseUrl/index.jsx
@@ -4,24 +4,33 @@ const API_BASE_URL = "https://api.artisanvillage.fr/api";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
   (config) => {
-    // Retrieves the access token stored in the browser's localStorage
     const token = localStorage.getItem("access_token");
-
-    // If a token exists, add an Authorization header to the request
     if (token) {
-      // The header format is "Bearer <token>", used for authentication
       config.headers.Authorization = `Bearer ${token}`;
     }
-
-    // Returns the query configuration (possibly modified) to continue the process
     return config;
   },
   (error) => {
-    // If an error occurs before the request is sent, it is rejected and processed
     return Promise.reject(error);
   }
 );
+
+export const login = async (email, password) => {
+  try {
+    const response = await api.post("/login", {
+      email,
+      password,
+    });
+    console.log("Connexion réussie:", response.data);
+    // Traitement du token ou des données de l'utilisateur
+    localStorage.setItem("access_token", response.data.token); // Remplacez par la clé correcte
+  } catch (error) {
+    console.error("Erreur lors de la connexion:", error);
+    // Gérer l'erreur ici (afficher un message à l'utilisateur, etc.)
+  }
+};
